378be03d508c470520c74c5eac82a140bd7885fd
G Tech Group <95207570+gtechgroupit@users.noreply.github.com>
2025-05-26 10:15:43 +0200

    Create plesk_integration_lang.php

---
+++ language/italian/plesk_integration_lang.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['plesk_integration'] = 'Plesk Integration';
+$lang['plesk_dashboard'] = 'Dashboard Plesk';
+$lang['plesk_websites'] = 'Siti Web';
+$lang['plesk_settings'] = 'Impostazioni Plesk';
+$lang['plesk_website_details'] = 'Dettagli Sito Web';
+
+// Messaggi di errore
+$lang['plesk_connection_error'] = 'Errore di connessione al server Plesk';
+$lang['plesk_api_error'] = 'Errore API Plesk';
+$lang['plesk_config_incomplete'] = 'Configurazione Plesk incompleta';
+$lang['plesk_website_not_found'] = 'Sito web non trovato';
+
+// Messaggi di successo
+$lang['plesk_connection_success'] = 'Connessione a Plesk riuscita!';
+$lang['plesk_settings_saved'] = 'Impostazioni Plesk salvate con successo';
+
+// Etichette campi
+$lang['plesk_server_url'] = 'URL Server Plesk';
+$lang['plesk_username'] = 'Username';
+$lang['plesk_password'] = 'Password';
+$lang['plesk_ssl_verify'] = 'Verifica SSL';
+$lang['plesk_cache_duration'] = 'Durata Cache';
+$lang['plesk_auto_sync'] = 'Sincronizzazione Automatica';
+$lang['plesk_debug_mode'] = 'Modalità Debug';
+
+// Stati
+$lang['plesk_status_active'] = 'Attivo';
+$lang['plesk_status_suspended'] = 'Sospeso';
+$lang['plesk_status_disabled'] = 'Disabilitato';
